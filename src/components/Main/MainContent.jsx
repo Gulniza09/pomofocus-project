5c38bb0aafe25a127dcbcfaefae43261174b9b57
Gulniza <gulnizajanyshova@gmail.com>
2022-04-25 12:17:03 +0300

    add function interval time

--- src/components/Main/MainContent.jsx
+++ src/components/Main/MainContent.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-alert */
 /* eslint-disable consistent-return */
@@ -6,54 +8,69 @@ import Input from '../UI/Input'
 import TimerContent from './TimerContent'
 import styled from 'styled-components'
 import { SettingProvider } from '../../context/SettingContext'
-
+import finishAudio from '../../assets/audio/asd.mp3'
+import useSound from 'use-sound'
+import { options } from '../../utils/constants/general'
 const MainContent = () => {
-   const { settingTime, setSettingTime } = useContext(SettingProvider)
-   const [pomodoro, setPomodoro] = useState(Number(settingTime.pomodoro))
-   const [shortBreak, setShortBreak] = useState(Number(settingTime.shortBreak))
-   const [longBreak, setLongBreak] = useState(Number(settingTime.longBreak))
-   const [content, setContent] = useState(0)
+   const {
+      settingTime,
+      setSettingTime,
+      setBackground,
+      setPlatform,
+      platform,
+      pomodoroCount,
+      setPomodoroCount,
+      ticking,
+      setTicking,
+   } = useContext(SettingProvider)
+   const [longBreakIntervalCount, setLongBreakIntervalCount] = useState(1)
    const [minutes, setMinutes] = useState(0)
    const [seconds, setSeconds] = useState(0)
-   const [ticking, setTicking] = useState(true)
-
-   useEffect(() => {
-      setPomodoro(settingTime.pomodoro)
-      setLongBreak(settingTime.longBreak)
-      setShortBreak(settingTime.shortBreak)
-   }, [settingTime])
-
+   const { pomodoro, longBreak, shortBreak, longBreakInterval } = settingTime
    const switchContent = (index) => {
-      setContent(index)
+      setPlatform(index)
    }
-   
+   const [finishPlay] = useSound(finishAudio)
    const getTickingTime = useCallback(() => {
       const timeStage = {
          0: pomodoro,
          1: shortBreak,
          2: longBreak,
       }
-      const seconds = (timeStage[content] * 3600) / 60
+      const seconds = (timeStage[platform] * 3600) / 60
       const time = `${Math.floor(seconds / 60)}:${
          seconds % 60 > 9 ? seconds % 60 : `0${seconds % 60}`
       }`
       const [minute, second] = time.split(':')
       setMinutes(Number(minute))
       setSeconds(Number(second))
-   }, [pomodoro, shortBreak, longBreak, content])
-
+   }, [pomodoro, shortBreak, longBreak, platform])
    useEffect(() => {
       getTickingTime()
    }, [getTickingTime])
-
+   const switchPlatformHandler = useCallback(() => {
+      if (platform === 0) {
+         setPomodoroCount((count) => count + 1)
+         if (longBreakIntervalCount === Number(longBreakInterval)) {
+            return setPlatform(2)
+         }
+         setPlatform(1)
+         setLongBreakIntervalCount((count) => count + 1)
+      } else if (platform === 1) {
+         setPlatform(0)
+      } else if (platform === 2) {
+         setPlatform(0)
+      }
+   }, [platform, longBreakInterval, longBreakIntervalCount])
    useEffect(() => {
       if (ticking) {
          return
       }
       if (!ticking) {
          const timer = setInterval(() => {
-            if (minutes === 0 && seconds === 0) {
-               alert('time up')
+            if (minutes === 0 && seconds === 1) {
+               finishPlay()
+               switchPlatformHandler()
             } else if (seconds === 0) {
                setMinutes(minutes - 1)
                setSeconds(59)
@@ -63,12 +80,15 @@ const MainContent = () => {
          }, 1000)
          return () => clearInterval(timer)
       }
-   }, [ticking, minutes, seconds])
-
+   }, [ticking, minutes, seconds, switchPlatformHandler])
+   useEffect(() => {
+      setBackground(options[platform].background)
+      getTickingTime()
+   }, [platform])
    return (
       <TimeContent>
          <TimerContent
-            content={content}
+            platform={platform}
             switchContent={switchContent}
             getTickingTime={getTickingTime}
             minutes={minutes}
@@ -77,14 +97,16 @@ const MainContent = () => {
             setTicking={setTicking}
          />
          <TimeFocus>
-            <p>#1</p>
+            <p>#{pomodoroCount}</p>
             <p>Time to focus!</p>
          </TimeFocus>
          <TaskName>
             <h3>Tasks</h3>
-            <img src="	https://pomofocus.io/icons/threedots-white.png" alt="" />
+            <img
+               src="  https://pomofocus.io/icons/threedots-white.png"
+               alt=""
+            />
          </TaskName>
-
          <Input className="add_tasks_input" placeholder="+  Add Task" />
       </TimeContent>
    )
--- src/context/SettingContext.jsx
+++ src/context/SettingContext.jsx
@@ -1,16 +1,20 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import React, { createContext, useState } from 'react'
-
 export const SettingProvider = createContext()
-
 const SettingContextProvider = (props) => {
    const [settingTime, setSettingTime] = useState({
-      pomodoro: 0.2,
+      pomodoro: 0.1,
       shortBreak: 0.3,
       longBreak: 1,
+      longBreakInterval: 1,
    })
+   const [platform, setPlatform] = useState(0)
    const [background, setBackground] = useState('rgb(217, 85, 80)')
    const [isActive, setIsActive] = useState('pomodoro')
+   const [autoStartBreaks, setAutoStartBreaks] = useState(false)
+   const [autoStartPomo, setAutoStartPomo] = useState(false)
+   const [pomodoroCount, setPomodoroCount] = useState(1)
+   const [ticking, setTicking] = useState(true)
    return (
       // eslint-disable-next-line react/jsx-no-constructed-context-values
       <SettingProvider.Provider
@@ -21,11 +25,20 @@ const SettingContextProvider = (props) => {
             setIsActive,
             background,
             setBackground,
+            autoStartBreaks,
+            setAutoStartBreaks,
+            autoStartPomo,
+            setAutoStartPomo,
+            platform,
+            setPlatform,
+            pomodoroCount,
+            setPomodoroCount,
+            ticking,
+            setTicking,
          }}
       >
          {props.children}
       </SettingProvider.Provider>
    )
 }
-
 export default SettingContextProvider
